e93a36e7f0a774bc0a1479b55b39cae6249530ab
Ittirit Thipphinit <itchampclub@gmail.com>
2019-01-30 16:17:57 +0700

    Update onoff.php

--- onoff.php
+++ onoff.php
@@ -1,9 +1,10 @@
 <?php
 $txt = "data.txt"; 
-if(isset($_POST["onoff"])) { // check if both fields are set
+if(isset($_POST["hidden_gender"])) { // check if both fields are set
     $fh = fopen($txt, 'a'); 
-    $txt=$_POST['onoff']; 
+    $txt=$_POST['hidden_gender']; 
     fwrite($fh,$txt); // Write information to the file
     fclose($fh); // Close the file
 }
 ?>
+
