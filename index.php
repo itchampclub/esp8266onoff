e49d0c5ac1eb8801fa9ec8656ec3003d88e94576
Ittirit Thipphinit <itchampclub@gmail.com>
2019-01-30 16:25:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,85 +1,46 @@
-<?php
-//index.php
-?>
-<!DOCTYPE html>
-<html>
- <head>
-  <title>Make Stylish Toggles Checkboxes  & Use in Form with PHP Ajax</title>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
-  <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
- </head>
- <body>
-  <br /><br />
-  <div class="container" style="width:600px;">
-   <h2 align="center">Make Stylish Toggles Checkboxes & Use in Form with PHP Ajax</h2><br /><br />
-   <form method="post" id="insert_data">
-    <div class="form-group">
-     <label>Enter Name</label>
-     <input type="text" name="name" id="name" class="form-control" />
-    </div>
-    <div class="form-group">
-     <label>Define Gender</label>
-     <div class="checkbox">
-      <input type="checkbox" name="gender" id="gender" checked />
-     </div>
-    </div>
-    <input type="hidden" name="hidden_gender" id="hidden_gender" value="Male" />
-    <br />
-    <input type="submit" name="insert" id="action" class="btn btn-info" value="Insert" />
-   </form>
+  <html>
+  <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script type="text/javascript"     src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script type="text/javascript">window.jQuery || document.write('<script src="classes/commons/jquery/jquery-1.7.1.min.js"><\/script>')</script>
+    <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
+    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+    <script src="http://malsup.github.com/jquery.form.js"></script> 
+
+
+  </head>
+  <body>
+    <form id="myForm" name="myForm" action="audio_alarm.php" method="post"> 
+      <input type="checkbox" name="toggle" id="toggle" data-toggle="toggle" data-off="Disabled" data-on="Enabled" checked>
+    </form>
+    <br><br>
+    <div class="panel panel-default">
+    <div class="panel-heading" id="heading"></div>
+    <div class="panel-body" id="body"></div>
   </div>
- </body>
-</html>
-
-<script>
-$(document).ready(function(){
- 
- $('#gender').bootstrapToggle({
-  on: 'Male',
-  off: 'Female',
-  onstyle: 'success',
-  offstyle: 'danger'
- });
-
- $('#gender').change(function(){
-  if($(this).prop('checked'))
-  {
-   $('#hidden_gender').val('Male');
-  }
-  else
-  {
-   $('#hidden_gender').val('Female');
-  }
- });
-
- $('#insert_data').on('submit', function(event){
-  event.preventDefault();
-  if($('#name').val() == '')
-  {
-   alert("Please Enter Name");
-   return false;
-  }
-  else
-  {
-   var form_data = $(this).serialize();
-   $.ajax({
-    url:"onoff.php",
-    method:"POST",
-    data:form_data,
-    success:function(data){
-     if(data == 'done')
-     {
-      $('#insert_data')[0].reset();
-      $('#gender').bootstrapToggle('on');
-      alert("Data Inserted");
-     }
-    }
-   });
-  }
- });
-
-});
-</script>
+    <div></div>
+    <script>
+      $('#toggle').change(function(){
+        var mode= $(this).prop('checked');
+        // // submit the form 
+        // $(#myForm).ajaxSubmit(); 
+        // // return false to prevent normal browser submit and page navigation 
+        // return false; 
+        $.ajax({
+          type:'POST',
+          dataType:'JSON',
+          url:'audio_alarm.php',
+          data:'mode='+mode,
+          success:function(data)
+          {
+            var data=eval(data);
+            message=data.message;
+            success=data.success;
+            $("#heading").html(success);
+            $("#body").html(message);
+          }
+        });
+      });
+    </script>
+  </body>
+  </html>
