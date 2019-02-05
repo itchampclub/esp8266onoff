ab71b5cec225e3d5ddd91ae4364b55073b0a03ab
Ittirit Thipphinit <itchampclub@gmail.com>
2019-02-05 08:18:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-
 <html>
   <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script type="text/javascript"     src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
     <script type="text/javascript">window.jQuery || document.write('<script src="classes/commons/jquery/jquery-1.7.1.min.js"><\/script>')</script>
     <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
-    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
+    <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.0/js/bootstrap-toggle.min.js"></script>
     <script src="http://malsup.github.com/jquery.form.js"></script> 
 
 
@@ -24,7 +23,7 @@ if ($read == "On")
     </br></br></br>
     <center>
     <form id="myForm" name="myForm" action="onoff.php" method="post"> 
-      <input type="checkbox" name="toggle" id="toggle" data-toggle="toggle" data-off="Off" data-on="On" <?php echo $status; ?> >
+      <input type="checkbox" name="toggle" id="toggle" data-toggle="toggle" data-toggle="toggle" data-onstyle="success" data-offstyle="danger" <?php echo $status; ?> >
     </form>
     <br><br>
     <div class="panel panel-default">
